a5e923e20a0ac3883282e1f10212a31ec07a0e96
wangyao <13564118478@163.com>
2019-07-01 16:08:29 +0800

    修改
    哈哈

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.7
+FROM nginx:latest
+ADD requirements.txt /tmp/requirements.txt
+RUN pip install -r /tmp/requirements.txt
+RUN mkdir /code
+WORKDIR /code
+COPY . /code
+COPY docker-entrypoint.sh docker-entrypoint.sh
+RUN chmod +x docker-entrypoint.sh
+RUN apt-get install uwsgi
+RUN apt-get install libfontconfig
+CMD /code/docker-entrypoint.sh
---
+++ sightseeing.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/jinqu/<num>')
+def jinqu(num):
+    print(num,'i am {}'.format(num))
+    vnc_url = 'http://www.guimp.com/'
+    return render_template('./test.html', vnc_url=vnc_url)
+    # return json.dumps({'state': 1})
+
+
+if __name__ == '__main__':
+    app.run(port=5010)
